4d160222ae44e1fdfe3ff7f8ca643a664d54bda6
Thirupathichalla-arch <91534651+Thirupathichalla-arch@users.noreply.github.com>
2021-12-22 21:54:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 
 import ("./index.css")
-import("./index.css")
-import Icon from './file.jpg';
 
-function component() {
 
-    
-    const element = document.createElement('div');
-    element.innerHTML ="hello world";
-    element.classList.add('red');
-    const myIcon = new Image();
-     myIcon.src = Icon;
-     element.appendChild(myIcon);
-     myIcon.classList.add('img');
-    
-    return element;
-  }
- 
-  document.body.appendChild(component());
+let logo = document.querySelector(".logo");
+
+logo.innerHTML = `<img src = "./imag/file.jpg"/>`;
+
+let Page = document.getElementById("page");
+
+let out = document.getElementById("in");
+let button = document.getElementById("button");
+
+button.onclick = ()=>{
+    printNotes();
+}
+
+function printNotes(){
+    Page.innerHTML = null;
+    let p = document.createElement('p');
+    p.classList.add("con");
+    p.innerText = out.value;
+    Page.append(p);
+}
